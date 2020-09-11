831189f938a6a3841e93c34c48d8245aebeb6160
mangoomeh <67454525+mangoomeh@users.noreply.github.com>
2020-09-11 10:22:18 +0800

    Update clash.py

--- clash.py
+++ clash.py
@@ -6,7 +6,7 @@ import pytz
 import math
 
 # Settings for API ================================================================
-clanTag = 'L2208GR9'
+clanTag = ''
 key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijc2YWYzMGJmLWFjYzgtNGE0Ny1hZmU2LWIwZjE0NzY2ZWNlYyIsImlhdCI6MTU5MjMxMzY0OSwic3ViIjoiZGV2ZWxvcGVyL2JjNzVkYTRmLTEyMGItOWU3Ny0xMTA0LWM0YmQxMDllMDc5OCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMjguMTI4LjEyOC4xMjgiXSwidHlwZSI6ImNsaWVudCJ9XX0.czFtTMv7pqaziRUiivFYyXdvwAvPQNpI7w9tNvrExj0cvzYFl20GHtdLL3LiVKM-ZUFs1wTXeSqfjXgygssT2g'
 base_url = "https://proxy.royaleapi.dev/v1"
 header = {"Authorization": "Bearer %s" %key}
@@ -38,15 +38,15 @@ def makeListOfdict_cLB(dict_clanData, dict_riverRaceData):
 	for dict_m in dict_clanData['items']:
 		dict_m_new = {}
 					# New 				# Old
-		dict_m_new['tag']		= dict_m['tag']
-		dict_m_new['name'] 		= dict_m['name']
-		dict_m_new['rank'] 		= dict_m['role'].title()
-		dict_m_new['lastSeen'] 	= dp.isoparse(dict_m['lastSeen']).astimezone(pytz.timezone('Asia/Singapore')).strftime("%d/%m %H:%M")
+		dict_m_new['tag']		= dict_m.get('tag')
+		dict_m_new['name'] 		= dict_m.get('name')
+		dict_m_new['rank'] 		= dict_m.get('role').title()
+		dict_m_new['lastSeen'] 	= dp.isoparse(dict_m.get('lastSeen')).astimezone(pytz.timezone('Asia/Singapore')).strftime("%d/%m %H:%M")
 		dict_m_new['arena'] 	= dict_m['arena']['name']
-		dict_m_new['trophy'] 	= dict_m['trophies']
-		dict_m_new['donate'] 	= dict_m['donations']
-		dict_m_new['received'] 	= dict_m['donationsReceived']
-		dict_m_new['lvl'] 		= dict_m['expLevel']
+		dict_m_new['trophy'] 	= dict_m.get('trophies')
+		dict_m_new['donate'] 	= dict_m.get('donations')
+		dict_m_new['received'] 	= dict_m.get('donationsReceived')
+		dict_m_new['lvl'] 		= dict_m.get('expLevel')
 		
 		for dict_participant in dict_riverRaceData['clan']['participants']:
 			if dict_m['tag'] == dict_participant['tag']:
@@ -67,29 +67,31 @@ def makeListOfdict_rRLB(dict_riverRaceData):
 	for dict_clan in dict_riverRaceData['clans']:
 		dict_clan_new = {}
 					 # New 				   # Old
-		dict_clan_new['tag'] 	= dict_clan['tag']
-		dict_clan_new['name'] 	= dict_clan['name']
-		dict_clan_new['fame'] 	= dict_clan['fame']
-		dict_clan_new['rp'] 	= dict_clan['repairPoints']
-		dict_clan_new['trophy'] = dict_clan['clanScore']
+		dict_clan_new['tag'] 	= dict_clan.get('tag')
+		dict_clan_new['name'] 	= dict_clan.get('name')
+		dict_clan_new['fame'] 	= dict_clan.get('fame')
+		dict_clan_new['rp'] 	= dict_clan.get('repairPoints')
+		dict_clan_new['trophy'] = dict_clan.get('clanScore')
 		list_riverRace.append(dict_clan_new)
 	return list_riverRace
 
-def makeListOfdict_rRCLB(dict_riverRaceData):
+def makeListOfClans_rR(dict_riverRaceData):
 	'''This block of code takes in raw dictionary of river race data
 	and convert it to a list of dictionaries containing only info about
 	clans and their war participants. This list is returned.'''
 
-	'''KEYS(S) ADDED TO RIVERRACE INDIVIDUAL'S DATA:'''
-	list_individualClanMembers = []
+	'''KEYS(S) ADDED TO RIVERRACE INDIVIDUAL'S DATA: name, fame+rp'''
+	listOfClans = []
 	for dict_clan in dict_riverRaceData['clans']:
+		listOfMembers = []
 		for dict_i in dict_clan['participants']:
 			dict_i_new = {}
-			dict_i_new['clan'] = dict_clan['name']
-			dict_i_new['name'] = dict_i['name']
-			dict_i_new['fame+rp'] = dict_i['fame'] + dict_i['repairPoints']
-			list_individualClanMembers.append(dict_i_new)
-	return list_individualClanMembers
+			dict_i_new['clan'] = dict_clan.get('name')
+			dict_i_new['name'] = dict_i.get('name')
+			dict_i_new['fame+rp'] = dict_i.get('fame') + dict_i.get('repairPoints')
+			listOfMembers.append(dict_i_new)
+		listOfClans.append(listOfMembers)
+	return listOfClans
 
 
 def sortListOfDict(listOfDict, keyToSortBy):
@@ -155,7 +157,7 @@ def cwgReview(listOfDict):
 
 # Review Rubric RBG
 def rbgReview(listOfDict):
-	# KEY(S) ADDED: 'rbrv'
+	# KEY(S) ADDED: 'review(bool)'
 	
 	# Rubric Settings ===================================
 	rubric = {
@@ -178,14 +180,14 @@ def rbgReview(listOfDict):
 		r = dict_m.get('rank')
 		for krb, vrb in rubric.items():
 			if d < rubric['m']['d'] or f < rubric['m']['f']:
-				dict_m['rbrv'] = 'fail'
+				dict_m['review(bool)'] = 'fail'
 				break
 			elif d >= vrb['d'] and f >= vrb['f']:
 				if r in review.keys():
-					dict_m['rbrv'] = review[r][krb]
+					dict_m['review(bool)'] = review[r][krb]
 					break
 				else:
-					dict_m['rbrv'] = krb
+					dict_m['review(bool)'] = krb
 					break
 
 		list_membersData.append(dict_m)
@@ -252,7 +254,7 @@ def clanLeaderboard(keys_to_call):
 							received, lvl, fame, rp, fame+rp'''
 		list_membersData = makeListOfdict_cLB(dict_clanData, dict_riverRaceData)
 		
-		# KEY(S) ADDED: 'ag', 'dg', 'rrg', 'ovg', 'rbrv', 'review'
+		# KEY(S) ADDED: 'ag', 'dg', 'rrg', 'ovg', 'review(bool)', 'review'
 		cwgReview(list_membersData)
 		rbgReview(list_membersData)
 		dReview(list_membersData)
@@ -268,6 +270,7 @@ def clanLeaderboard(keys_to_call):
 
 		# Print block
 		print('\nMembers: {}/50'.format(len(list_membersData)))
+		print('Fame: {}/50000'.format(dict_riverRaceData['clan'].get('fame')))
 		print(formatString_listOfDict(list_membersData, keys_to_call))
 		e = input("<Enter> to refresh. 'e' to return to menu. Otherwise:\n{}Your Choice: ".format(string_sortMsg))
 		if e.lower() == 'e':
@@ -318,46 +321,20 @@ def riverRaceLeaderboard(keys_to_call):
 		if e.lower() == 'e':
 			break
 
-def individualClanMembersLeaderboard(keys_to_call):
-	# Initialize variables
-	e = ''
-	stored_e = ''
-	
-	# Sort Settings =================================
-	list_sortCriteria = ['name', 'fame+rp', 'clan']
-	# ===============================================
-
-	# This block check if sort criterion is in keys to call and recreate itself
-	list_temp = []
-	for i in range(len(list_sortCriteria)):
-		if list_sortCriteria[i] in keys_to_call:
-			list_temp.append(list_sortCriteria[i])
-	list_sortCriteria = list_temp
-
-	# This block generates sort message
-	string_sortMsg = ''
-	for i in range(len(list_sortCriteria)):
-		string_sortMsg += '{}. Sort by {}\n'.format(i+1, list_sortCriteria[i].title())
+def riverRaceClanMembers(keys_to_call):
 
 	# This block is the interface
 	while True:
 		#Retrieve Data
 		dict_riverRaceData = retrieve_currentRiverRace()
-		
-		'''KEY(S) ADDED: 	fame, rp, trophy, tag, name'''
-		list_individualClanMembers = makeListOfdict_rRCLB(dict_riverRaceData)
+		listOfClans = makeListOfClans_rR(dict_riverRaceData)
 
-		# This block checks if sort is needed and sort if needed
-		if e in [str(i) for i in range(1,len(list_sortCriteria)+1)]:
-			stored_e = e
-		for i in range(len(list_sortCriteria)):
-			if stored_e == str(i+1):
-				list_individualClanMembers = sortListOfDict(list_individualClanMembers, list_sortCriteria[i])
-				break
+		for clan in listOfClans:
+			clan = sortListOfDict(clan, 'fame+rp')
+			print('\nClan: {}'.format(clan[0].get('clan')), formatString_listOfDict(clan, keys_to_call))
 
 		# Print block
-		print(formatString_listOfDict(list_individualClanMembers, keys_to_call))
-		e = input("<Enter> to refresh. 'e' to return to menu. Otherwise:\n{}Your Choice: ".format(string_sortMsg))
+		e = input("<Enter> to refresh. 'e' to return to menu.")
 		
 		# Check if user wants to return to menu
 		if e.lower() == 'e':
@@ -392,6 +369,7 @@ def fameCalculator():
 	print(f'If win all battles: {totalLvl*2*e}\n')
 
 def main():
+	global clanTag
 	# Settings for table ==============================================================
 	'''Available keys: 	| tag | name | rank | lastSeen | arena | trophy | donate | received |
 						|lvl | fame | rp | fame+rp |'''
@@ -404,44 +382,73 @@ def main():
 					 'fame+rp', 
 					 'rank']
 	#keysToCall_mD.extend(['ag', 'dg', 'rrg', 'ovg']) # CWG REVIEW 
-	keysToCall_mD.extend(['rbrv']) # RB REVIEW
+	keysToCall_mD.extend(['review(bool)']) # RB REVIEW
 	#keysToCall_mD.extend(['review']) # 30 donates only review
 	keysToCall_mD.extend(['name'])
-	keysToCall_rR = ['fame', 'rp', 'trophy', 'tag', 'name']
-	keysToCall_rRI = ['clan', 'fame+rp', 'name']
+	
+	keysToCall_rR = ['fame',
+					 'rp',
+					 'trophy',
+					 'tag',
+					 'name']
+	
+	keysToCall_rRI = ['fame+rp', 'name']
 	# =================================================================================
 
 	# Menu Interface
 	while True:
-		option = input(
-			'1. Clan Leaderboard\n'
-			'2. River Race Leaderboard\n'
-			'3. Print raw clan data dictionary\n'
-			'4. Print raw river race data dictionary\n'
-			'5. Check available keys\n'
-			'6. Fame calculator\n'
-			'7. Clans Individual Members Fame\n'
-			'0. Exit\n'
-			'Your Choice: '
-			)
-		if option == '1':
-			clanLeaderboard(keysToCall_mD)
-		elif option == '2':
-			riverRaceLeaderboard(keysToCall_rR)
-		elif option == '3':
-			print(retrieve_clanData())
-		elif option == '4':
-			print(retrieve_currentRiverRace())
-		elif option == '5':
-			checkAvailableKeys()
-		elif option == '6':
-			fameCalculator()
-		elif option == '7':
-			individualClanMembersLeaderboard(keysToCall_rRI)
-		elif option == '0':
-			break
-		
-		else:
-			print('Invalid. Try again.')
+		while True:
+			choice = input('\nClan Manager v1\n'
+							   '1. Spontane Main Clan\n'
+							   '2. Spontane Xp1\n'
+							   '0. Exit\n'
+							   'Your Choice: ')
+			if choice == '1':
+				clanTag = 'L2208GR9'
+				break
+			elif choice == '2':
+				clanTag = 'YPQ8CQ0R'
+				break
+			elif choice == '0':
+				exit()
+			else:
+				print('Invalid Choice. Try again.')
+
+		while True:
+			option = input(
+				'\nClash Royale Leaderboards/Tools:\n'
+				'1. Clan Leaderboard\n'
+				'2. River Race Leaderboard\n'
+				'3. River Race Clans Leaderboard\n'
+				'4. Fame calculator\n'
+				'0. Return to Main Menu\n'
+				'\nMaintenance Purpose:\n'
+				'5. Print raw clan data dictionary\n'
+				'6. Print raw river race data dictionary\n'
+				'7. Check available keys (w/o review)\n'
+				'Your Choice: '
+				)
+			
+			if option == '1':
+				clanLeaderboard(keysToCall_mD)
+			elif option == '2':
+				riverRaceLeaderboard(keysToCall_rR)
+			elif option == '3':
+				riverRaceClanMembers(keysToCall_rRI)
+			elif option == '4':
+				fameCalculator()
+			elif option == '5':
+				print(retrieve_clanData())
+			elif option == '6':
+				print(retrieve_currentRiverRace())
+			elif option == '7':
+				checkAvailableKeys()
+			
+			
+			elif option == '0':
+				break
+			
+			else:
+				print('Invalid. Try again.')
 
 main()
